f0ec179f22431bc8d9d881959b3fb98c41bdf05f
David Hoang <dvhoang@amazon.com>
2023-05-11 13:28:34 -0500

    updated shell script and createqsobjects removed delete section

--- qsdeploy.sh
+++ qsdeploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo $parent_path
 cd "$parent_path/cdk-lakeformation-permissions/source"
-echo $(pwd)
-echo "Running cdk deploy --all command"
+echo "Running cdk synth and deploy --all command"
 echo "--------------------------------------------"
 echo " "
+cdk synth
 cdk deploy --all -v
 echo "Finished cdk deploy"
 echo "--------------------------------------------"
-
-# echo "Running QuickSight Generate Dashboards"
-# echo "--------------------------------------------"
-# echo " "
-# python3 createobjects.py
-# echo "Finished QuickSight Generate Dashboards"
-# echo "--------------------------------------------"
+echo "Running QuickSight Generate Dashboards"
+echo "--------------------------------------------"
+echo " "
+python3 ../../scripts/createqsobjects.py
+echo "Finished QuickSight Generate Dashboards"
+echo "--------------------------------------------"
